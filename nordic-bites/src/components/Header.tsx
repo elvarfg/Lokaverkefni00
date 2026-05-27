fde3b642f4193fff24cd5ede889e4a19c29b6fe0
elvarfg <elvarfg@gmail.com>
2026-05-27 22:20:56 +0000

    hero section

--- nordic-bites/src/components/Header.tsx
+++ nordic-bites/src/components/Header.tsx
@@ -1,9 +1,19 @@
 import { NavLink } from "react-router-dom";
 
+import logo from "../assets/images/nordicbiteslogo.png";
+
 function Header() {
   return (
     <header className="header">
-      <h1>Nordic Bite</h1>
+      <div className="logo-container">
+        <img
+          src={logo}
+          alt="Nordic Bite logo"
+          className="logo"
+        />
+
+        <h1>Nordic Bite</h1>
+      </div>
 
       <nav>
         <NavLink to="/">Home</NavLink>
