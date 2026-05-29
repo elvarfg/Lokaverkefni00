b5eb695c4822ac5f6881f38ed6fdc1e3bdd22ada
elvarfg <elvarfg@gmail.com>
2026-05-29 17:16:23 +0000

    Hamburger Menu Mobile

--- nordic-bites/src/components/Header.tsx
+++ nordic-bites/src/components/Header.tsx
@@ -1,24 +1,47 @@
+import { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 
 import logo from "../assets/images/nordicbiteslogo.png";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function closeMenu() {
+    setMenuOpen(false);
+  }
+
   return (
     <header className="header">
-      <Link to="/" className="logo-container">
-        <img
-          src={logo}
-          alt="Nordic Bite logo"
-          className="logo"
-        />
-
+      <Link to="/" className="logo-container" onClick={closeMenu}>
+        <img src={logo} alt="Nordic Bite logo" className="logo" />
       </Link>
 
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/menu">Menu</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+      <button
+        className="hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Open navigation menu"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <nav className={menuOpen ? "nav nav-open" : "nav"}>
+        <NavLink to="/" onClick={closeMenu}>
+          Home
+        </NavLink>
+
+        <NavLink to="/menu" onClick={closeMenu}>
+          Menu
+        </NavLink>
+
+        <NavLink to="/about" onClick={closeMenu}>
+          About
+        </NavLink>
+
+        <NavLink to="/contact" onClick={closeMenu}>
+          Contact
+        </NavLink>
       </nav>
     </header>
   );
